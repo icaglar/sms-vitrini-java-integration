313fdae3ce73efcba31cfc3d44a133670b3171a8
icaglar <bizzatibrahim@gmail.com>
2017-02-21 23:09:52 +0300

    Update SmsVitriniTest.java

--- examples/SmsVitriniTest.java
+++ examples/SmsVitriniTest.java
@@ -1,1 +1,21 @@
 
+public class SmsVitriniTest {
+  
+ SmsProvider smsProvider; 
+  
+ public static void main(String[] args) {
+		
+   	SmsModel smsModel = new SmsModelBuilder().addActionType(SmsActionEnum.SEND_SMS_STANDOLONE_OR_GROUP).addBody(prefix +"send text").addNumbers("numbers comma separated").build();
+		
+    SmsResult  smsResult = smsProvider.sendSms(smsModel);
+   
+   if (smsResult != null)
+   {
+									if (smsResult.getStatus())
+									{
+                    // do something if result is ok
+                  }
+   }
+	} 
+  
+}
