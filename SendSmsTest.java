5e564a9845f8ace5f404fa1484c8a86b5382ff68
icaglar <bizzatibrahim@gmail.com>
2017-02-21 23:05:14 +0300

    Delete SendSmsTest.java

--- SendSmsTest.java
+++
@@ -1,6 +0,0 @@
-
-
-	SmsProvider smsProvider;
-
-	SmsModel smsModel = new SmsModelBuilder().addActionType(SmsActionEnum.SEND_SMS_STANDOLONE_OR_GROUP).addBody(prefix + "send text").addNumbers("String array numbers").build();
-  SmsResult smsResult = smsProvider.sendSms(smsModel);
